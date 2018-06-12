8bb6a003fc6a55da40d3a33bc218fd7ae3884c04
1712244 <lucasalvesoliva@gmail.com>
2018-06-11 21:52:15 -0300

    Criado tela inicial do projeto (tela de login)

--- SAT/Controllers/Base/IBaseController.cs
+++ SAT/Controllers/Base/IBaseController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Models;
+﻿using Models;
 using System.Collections.Generic;
 
 namespace Controllers.Base
--- SAT/Models/Atendimento.cs
+++ SAT/Models/Atendimento.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class Atendimento
+    public class Atendimento
     {
         public int AtendimentoID { get; set; }
         public string Nome { get; set; }
